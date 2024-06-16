1c20bf8349dfd4eb41c9708c351837db539481b9
brenden-js <85804361+brenden-js@users.noreply.github.com>
2024-06-16 14:52:44 -0700

    removed inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
 import "@/app/globals.css";
 
-import {Inter} from "next/font/google";
 import {ClerkProvider} from "@clerk/nextjs";
 import {Toaster} from "@/components/ui/sonner"
 
-
-const inter = Inter({
-    subsets: ["latin"],
-    variable: "--font-sans",
-});
 
 export const metadata = {
     title: "Listing Lab",
@@ -20,7 +14,7 @@ export default function RootLayout({children}: { children: React.ReactNode; }) {
     return (
         <ClerkProvider>
             <html lang="en" className="h-full bg-white">
-            <body className={inter.className + " h-full"}>
+            <body className={" h-full"}>
             <main>{children}</main>
             <Toaster/>
             </body>
