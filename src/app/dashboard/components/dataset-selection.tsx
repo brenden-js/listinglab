9c6d66a765cb721b498b74086b9f159c8bad594b
brenden-js <85804361+brenden-js@users.noreply.github.com>
2024-07-25 10:28:56 -0700

    Made padding smaller.

--- src/app/dashboard/components/dataset-selection.tsx
+++ src/app/dashboard/components/dataset-selection.tsx
@@ -7,7 +7,7 @@ import clsx from "clsx";
 const DataButton = ({value, setDataset, currentSelectedValue, label}: { value: string, currentSelectedValue: string | undefined, setDataset: any, label: string }) => {
     return (
         <button
-            className={clsx("p-2 rounded-md mb-4 cursor-pointer hover:border-gray-400 duration-100 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2",
+            className={clsx("px-2 py-1 rounded-md mb-4 cursor-pointer hover:border-gray-400 duration-100 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2",
                 value === currentSelectedValue ? 'bg-gray-50 border-indigo-500 ring-1 ring-indigo-500 hover:border-indigo-600 border-2 border-transparent' : 'border-2 border-gray-300'
             )} value={value}
             onClick={() => setDataset(value)}>
