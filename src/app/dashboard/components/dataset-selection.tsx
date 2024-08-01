f1ea9e9eda35512228100f4d17cbce1184ccc23e
brenden-js <85804361+brenden-js@users.noreply.github.com>
2024-08-01 11:51:10 -0700

    Added a hover effect for less visual clutter.

--- src/app/dashboard/components/dataset-selection.tsx
+++ src/app/dashboard/components/dataset-selection.tsx
@@ -2,23 +2,20 @@
 import {useContext} from "react";
 import {CurrentPromptContext} from "@/app/dashboard/contexts/prompts";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
-import {Label} from "@/components/ui/label";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
+import {Card, CardDescription,  CardHeader, CardTitle} from "@/components/ui/card";
 
 export default function DatasetSelection() {
     const {dataset, setDataset} = useContext(CurrentPromptContext)
     return (
         <div className="flex w-full gap-2 mt-4">
-            <Tabs defaultValue="interior" className="w-[700px]">
+            <Tabs defaultValue="interior" className="w-[700px] group">
                 <TabsList className="grid w-full grid-cols-3">
                     <TabsTrigger value="interior" onClick={() => setDataset('interior')}>House Data</TabsTrigger>
                     <TabsTrigger value="exterior" onClick={() => setDataset('exterior')}>Neighborhood Data</TabsTrigger>
                     <TabsTrigger value="investment" onClick={() => setDataset('investment')}>Mortgage & Investment Data</TabsTrigger>
                 </TabsList>
                 <TabsContent value="interior">
-                    <Card>
+                    <Card className="transition-opacity duration-300 opacity-70 group-hover:opacity-100 hover:opacity-100">
                         <CardHeader>
                             <CardTitle>House and listing details</CardTitle>
                             <CardDescription>
@@ -29,7 +26,7 @@ export default function DatasetSelection() {
                     </Card>
                 </TabsContent>
                 <TabsContent value="exterior">
-                    <Card>
+                    <Card className="transition-opacity duration-300 opacity-70 group-hover:opacity-100 hover:opacity-100">
                         <CardHeader>
                             <CardTitle>Neighborhood and business data</CardTitle>
                             <CardDescription>
@@ -41,7 +38,7 @@ export default function DatasetSelection() {
                     </Card>
                 </TabsContent>
                 <TabsContent value="investment">
-                    <Card>
+                    <Card className="transition-opacity duration-300 opacity-70 group-hover:opacity-100 hover:opacity-100">
                         <CardHeader>
                             <CardTitle>Mortgage and investment data</CardTitle>
                             <CardDescription>
