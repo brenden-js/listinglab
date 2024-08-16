8c50db1afdf1cd2cc6586f2c5464987a8c3cc8d2
brenden-js <85804361+brenden-js@users.noreply.github.com>
2024-08-15 20:41:32 -0700

    Made the card a little better, still looks off for some reason. WIll need to revisit at another time

--- src/app/dashboard/houses/page.tsx
+++ src/app/dashboard/houses/page.tsx
@@ -5,41 +5,106 @@ import {House} from "@/app/dashboard/contexts/prompts";
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
-import React, { useEffect, useState} from "react";
-import {HouseDialogProvider, } from "@/app/dashboard/contexts/house-dialog-context";
+import React, {useEffect, useState} from "react";
+import {HouseDialogProvider,} from "@/app/dashboard/contexts/house-dialog-context";
 import {HouseDialog} from "@/app/dashboard/houses/components/house-dialog";
 import {AddCityForm, CityCard} from "@/app/dashboard/houses/components/city-popover";
+import {ChatBubbleIcon} from "@radix-ui/react-icons";
 
 export interface ChatMessage {
     sender: string;
     message: string;
 }
+const HousePreviewCard = ({ house }: { house: House }) => {
 
-const HousePreviewCard = ({house}: { house: House }) => {
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  if (!house) return null;
 
+  const expertiseData = getExpertiseData(house);
+  const totalMessages =  expertiseData ? getTotalMessages(expertiseData) : 0;
+
+  return (
+    <>
+      <Card
+        className="h-full transition-all duration-300 hover:shadow-sm cursor-pointer"
+        onClick={() => setIsDialogOpen(true)}
+      >
+        {totalMessages > 0 && (
+          <MessageIndicator messages={totalMessages} />
+        )}
+        <CardHeader className="flex">
+          <CardTitle className="text-lg font-bold">
+            {house.stAddress}
+          </CardTitle>
+          <CardDescription className="text-sm text-gray-500">
+            {house.city}, {house.state} {house.zipCode}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex-grow">
+          <HouseDetails house={house} />
+        </CardContent>
+      </Card>
+      <HouseDialog house={house} open={isDialogOpen} onOpenChange={setIsDialogOpen} />
+    </>
+  );
+};
+
+const getExpertiseData = (house: House) => {
+    if (!house) return {
+        mainExpertise: 0,
+        locationExpertise: 0,
+        financialExpertise: 0,
+        propertyExpertise: 0,
+    }
+  const mainExpertise = house.mainExpertise ? JSON.parse(house.mainExpertise).filter((message: ChatMessage) => message.sender === 'You').length : 0;
+  const locationExpertise = house.locationExpertise ? JSON.parse(house.locationExpertise).filter((message: ChatMessage) => message.sender === 'You').length : 0;
+  const financialExpertise = house.financialExpertise ? JSON.parse(house.financialExpertise).filter((message: ChatMessage) => message.sender === 'You').length : 0;
+  const propertyExpertise = house.propertyExpertise ? JSON.parse(house.propertyExpertise).filter((message: ChatMessage) => message.sender === 'You').length : 0;
+
+  return {
+    mainExpertise,
+    locationExpertise,
+    financialExpertise,
+    propertyExpertise,
+  };
+};
+
+const getTotalMessages = (expertiseData: { [key: string]: number } | undefined) => {
+    if (!expertiseData) return 0;
+  return Object.values(expertiseData).reduce((acc, messages) => acc + messages, 0);
+};
+
+const MessageIndicator = ({ messages }: { messages: number }) => {
+  return (
+    <div className="relative">
+      <div
+        className="absolute text-gray-500 top-6 right-6 bg-gray-100 p-1 rounded-md flex justify-center items-center"
+      >
+        <ChatBubbleIcon className="pr-0.5 h-5 w-5 text-lg text-gray-400" />
+        <span className="text-sm font-semibold text-gray-500">{messages}</span>
+      </div>
+    </div>
+  );
+};
+
+const HouseDetails = ({ house }: { house: House }) => {
     if (!house) return null;
-
-    return (
-        <>
-            <Card
-                className="h-full transition-all duration-300 hover:shadow-sm cursor-pointer"
-                onClick={() => setIsDialogOpen(true)}
-            >
-                <CardHeader>
-                    <CardTitle>{house.stAddress}</CardTitle>
-                    <CardDescription>
-                        {house.city} {house.zipCode}
-                    </CardDescription>
-                </CardHeader>
-                <CardContent className="flex-grow">
-                    {`Listed at $${house.price?.toLocaleString()} Found at
-                    ${house.createdAt.toLocaleDateString()}`}
-                </CardContent>
-            </Card>
-            <HouseDialog house={house} open={isDialogOpen} onOpenChange={setIsDialogOpen}/>
-        </>
-    );
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      <div>
+        <span className="text-lg font-bold">${house.price?.toLocaleString()}</span>
+        <span className="text-sm text-gray-500 block">Listed at {house.createdAt.toLocaleDateString()}</span>
+      </div>
+      <div>
+        <span className="text-md">{house.beds} Beds | {house.baths} Baths</span>
+        <span className="text-md block">{house.sqft.toLocaleString()} Sqft</span>
+      </div>
+      <div>
+        <span className="text-md">{house.yearBuilt} Year Built</span>
+        <span className="text-md block">{house.status}</span>
+      </div>
+    </div>
+  );
 };
 
 const LoadingSkeletons = () => (
